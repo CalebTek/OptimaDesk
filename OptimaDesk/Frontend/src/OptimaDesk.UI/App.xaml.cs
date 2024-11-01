71e30807ac961dddeee6e45f3032befd371dbe54
CalebTek <olawale.odeyemi@decagon.dev>
2024-11-01 13:54:57 +0100

    Create App.xaml.cs

---
+++ OptimaDesk/Frontend/src/OptimaDesk.UI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace OptimaDesk.UI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
